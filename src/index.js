3c0f8fc82a3c2d7cc9f0953cb601148be411dfe8
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-02 09:38:22 +0530

    Create Fire Base Connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Updated message");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxfDib__VrFYO65tSK7mnd4Di7aczbfbo",
+  authDomain: "notnotion-131c3.firebaseapp.com",
+  projectId: "notnotion-131c3",
+  storageBucket: "notnotion-131c3.firebasestorage.app",
+  messagingSenderId: "953994329145",
+  appId: "1:953994329145:web:74af641e3cb95ff3b8e1e8"
+};
+
+const app = initializeApp(firebaseConfig);
